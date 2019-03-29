1ab09cdbf3ae97dfe238d4074f27a76a4f80675d
Raymart <tramyardg@gmail.com>
2019-03-29 00:54:18 -0400

    employee index minor UI fix

--- employee-index.php
+++ employee-index.php
@@ -167,7 +167,7 @@ $book = new Book();
                     <select multiple class="form-control" id="booksToOrder"></select>
                 </div>
                 <div class="form-group">
-                    <h4>Step 2: Enter number of books needed</h4>
+                    <h5>Step 3: Enter number of books needed</h5>
                     <label for="edition">Order Quantity</label>
                     <input type="number" class="form-control" id="edition">
                 </div>
@@ -178,7 +178,8 @@ $book = new Book();
         <div class="tab-pane fade" id="addBooks" role="tabpanel" aria-labelledby="addBooks-tab">
             <p><code>This tab is for adding new book that does <b>NOT</b> exists yet in the inventory. For extra marks.</code></p>
             <form>
-                <div class="row">
+                <div class="row mt-2">
+                    <!-- isbn -->
                     <div class="col">
                         <label for="isbn">ISBN</label>
                         <input type="text" maxlength="11" class="form-control" id="isbn">
@@ -188,27 +189,24 @@ $book = new Book();
                         <label for="title">Title</label>
                         <input type="text" class="form-control" id="title">
                     </div>
+                </div>
+                <div class="row mt-2">
                     <!-- edition -->
                     <div class="col">
                         <label for="edition">Edition</label>
                         <input type="number" class="form-control" id="edition">
                     </div>
-                </div>
-                <div class="row mt-2">
                     <!-- price -->
                     <div class="col">
                         <label for="price">Price</label>
                         <input type="number" class="form-control" id="price" step=any>
                     </div>
-                    <!-- publisher_id -->
+                </div>
+                <div class="row mt-2">
+                    <!-- publishers -->
                     <div class="col">
                         <label for="publishersSelect">Publisher</label>
                         <select class="form-control" id="publishersSelect"></select>
-                    </div>
-                    <!-- authors -->
-                    <div class="col">
-                        <label for="authorsSelect">Authors</label>
-                        <select multiple class="form-control" id="authorsSelect"></select>
                     </div>
                     <!-- Book Category -->
                     <div class="col">
@@ -223,7 +221,12 @@ $book = new Book();
                         </select>
                     </div>
                 </div>
-                <div class="row ">
+                <div class="row mt-2">
+                    <!-- authors -->
+                    <div class="col">
+                        <label for="authorsSelect">Authors</label>
+                        <select multiple class="form-control" id="authorsSelect"></select>
+                    </div>
                     <div class="col mb-2">
                         <label for="bookImageControl">Book Cover Image (optional)</label>
                         <input type="file" class="form-control-file" id="bookImageControl">
