985abd49e9d8bf7838a77563e7d19858fed3f6ca
Raymart De Guzman <tramyardg@gmail.com>
2019-03-30 18:35:55 -0400

    minor refactor js

--- employee-book-function.php
+++ employee-book-function.php
@@ -109,7 +109,6 @@ if (isset($_SESSION["employee"])) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-
     <script src="js/employee-book-features.js"></script>
     <script>
         $(document).ready(function () {
