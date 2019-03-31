899c0ae9a33fe70be212cb096d1a0df325ea63f5
Raymart De Guzman <tramyardg@gmail.com>
2019-03-31 10:54:10 -0400

    removed unused function in controller

--- controller/BookInventoryController.php
+++ controller/BookInventoryController.php
@@ -7,29 +7,20 @@ class BookInventoryController
     {
     }
 
-    public function fetchBookInventoryByBookId($id)
-    {
-        $sql = "SELECT * FROM books_inventory WHERE book_inv_id =?;";
-        $stmt = DB::getInstance()->prepare($sql);
-        $stmt->execute([$id]);
-        return $stmt->fetchAll(PDO::FETCH_CLASS, "BookInventory");
-    }
-
-    public function update(BookInventory $bkInventory)
-    {
-        $sql = "UPDATE `books_inventory` SET `qty_on_hand` = ? WHERE `book_id` = ?;";
-        $stmt = DB::getInstance()->prepare($sql);
-        // order follows '?' from $sql
-        $exec = $stmt->execute([$bkInventory->getQtyOnHand(), $bkInventory->getBookId()]);
-        echo json_encode(array('result' => $exec));
-    }
-
     public function updateByBookIdAndQtyReceived($bookId, $qtyReceived)
     {
         $sql = "UPDATE `books_inventory` SET `qty_on_hand` = `qty_on_hand` + ? WHERE `book_id` = ?;";
         $stmt = DB::getInstance()->prepare($sql);
         $exec = $stmt->execute([$qtyReceived, $bookId]);
         echo json_encode(array('result' => $exec));
+    }
+
+    public function save(BookInventory $inventory)
+    {
+        $sql = 'INSERT INTO `books_inventory` (`book_id`, `qty_on_hand`) VALUES (?, ?);';
+        $stmt = DB::getInstance()->prepare($sql);
+        $exec = $stmt->execute([$inventory->getBookId(), $inventory->getQtyOnHand()]);
+        echo json_encode(['result' => $exec]);
     }
 
 }
