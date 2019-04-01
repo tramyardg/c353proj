c27ddb4ce58fb2f0f07a321dae679c97d3581557
Raymart De Guzman <tramyardg@gmail.com>
2019-04-01 10:23:07 -0400

    removed unsecured http jquery

--- login.php
+++ login.php
@@ -103,9 +103,9 @@ if (isset($_SESSION["customer"]))
     feather.replace();
 </script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
     (function () {
         'use strict';
