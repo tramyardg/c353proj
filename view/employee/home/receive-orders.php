aabc0eb66e17183e8c580a2165614441709eb2b0
AloysPortafaix <aloys.portafaix@gmail.com>
2019-04-06 15:05:39 -0400

    Update receive-orders.php

--- view/employee/home/receive-orders.php
+++ view/employee/home/receive-orders.php
@@ -1,37 +1,33 @@
 <?php
-
-$shController = new BookstoreOrderController();
-$shipments = $shController->fetchShipmentsToReceive();
+require ('controller/OrderController.php');
+$oController = new OrderController();
+$orders = $oController->fetchAllReceivedOrders();
 ?>
 <div class="tab-pane fade show active" id="receive-shipment" role="tabpanel" aria-labelledby="receive-shipment-tab">
-    <table id="booksReceiveTable" class="table table-striped table-hover table-sm">
+    <table id="bookReceiveTable" class="display table table-striped table-hover table-sm">
         <thead>
-        <tr>
-            <th class="d-none">Order ID</th>
+        <tr>	
+            <th>Order ID</th>
             <th>Book ID</th>
             <th>Title</th>
             <th>ISBN</th>
             <th>Quantity</th>
             <th>Publisher ID & Name</th>
             <th>Date Shipped</th>
-            <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <?php // if (count($shipments)) {
-            //foreach ($shipments as $k) { ?>
-                <tr>
-                    <td class="d-none" data-id=<?php //echo $k['shipment_id']; ?>></td>
-                    <td><?php //echo $k['book_id']; ?></td>
-                    <td><?php //echo $k['book_title']; ?></td>
-                    <td><?php //echo $k['isbn']; ?></td>
-                    <td><?php //echo $k['qty_to_receive']; ?></td>
-                    <td><?php //echo $k['publisher_id'] . ' - ' . $k['company_name']; ?></td>
-                    <td><?php //echo $k['date_shipped']; ?></td>
-                    <td><?php //echo $k['is_received'] == '0' ? 'TO BE ADDED' : 'RECEIVED'; ?></td>
+        <?php for ($i = 0; $i < count($orders); $i++) { ?>
+                <tr>	
+                    <td><?php echo $orders[$i]['order_id']; ?></td>
+		    <td><?php echo $orders[$i]['book_id']; ?></td>
+                    <td><?php echo $orders[$i]['title']; ?></td>
+                    <td><?php echo $orders[$i]['isbn']; ?></td>
+                    <td><?php echo $orders[$i]['quantity']; ?></td>
+                    <td><?php echo $orders[$i]['publisher_id'], "-" , $orders[$i]['company_name']; ?></td>
+                    <td><?php echo $orders[$i]['order_date']; ?></td>
                 </tr>
-            <?php // }
-        //} ?>
+            <?php  } ?>
         </tbody>
         <tfoot>
         <tr>
@@ -42,17 +38,9 @@ $shipments = $shController->fetchShipmentsToReceive();
             <th>Quantity</th>
             <th>Publisher ID & Name</th>
             <th>Date Shipped</th>
-            <th>Status</th>
         </tr>
         </tfoot>
     </table>
-    <div class="my-3">
-        <div class="row">
-            <div class="col-6">
-                <button type="button" id="add-to-inventory" class="btn btn-primary btn-md">Receive</button>
-            </div>
-        </div>
-    </div>
     <!-- Receive Orders Modal -->
     <div class="modal fade" id="receiveShipmentModal" tabindex="-1" role="dialog"
          aria-hidden="true">
@@ -74,4 +62,39 @@ $shipments = $shController->fetchShipmentsToReceive();
             </div>
         </div>
     </div>
+    
 </div>
+
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/select/1.2.5/js/dataTables.select.min.js"></script>
+<script>
+
+$("#bookReceiveTable tr").click(function(){
+   $(this).addClass('selected').siblings().removeClass('selected');    
+   var value= $(this).find('td:first').html();
+
+   //alert(value);
+   let retVal = confirm("The order ID is " + value + ".\nDo you want to continue?");
+   if(retVal == true){
+	$.post("api/process.php", {updateReceiveItems: value}, (response) => {
+console.log(response);
+                    if(response) {
+                       console.log('success');
+                    } else {
+                       alert('something went wrong');
+                    }
+                });
+	return true;
+   } else {
+	document.write("User does not want to continue!");
+	return false;	
+   }
+});
+
+</script>
+
+
